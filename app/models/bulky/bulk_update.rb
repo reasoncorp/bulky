61d0fb17788b0f5062579bd0a4e4464da0aac0f8
Zach Colon <zach.colon@tma1.com>
2013-02-18 16:43:42 -0500

    model destroys dependents on update deletion

--- app/models/bulky/bulk_update.rb
+++ app/models/bulky/bulk_update.rb
@@ -1,4 +1,5 @@
 class Bulky::BulkUpdate < ActiveRecord::Base
+
   self.table_name = 'bulky_bulk_updates'
 
   serialize :updates, Hash
@@ -6,7 +7,7 @@ class Bulky::BulkUpdate < ActiveRecord::Base
 
   validates :ids, :updates, presence: true
 
-  has_many :updated_records, class_name: 'Bulky::UpdatedRecord', foreign_key: :bulk_update_id
+  has_many :updated_records, class_name: 'Bulky::UpdatedRecord', foreign_key: :bulk_update_id, dependent: :destroy
 
   validates_each :updates do |record, attr, value|
     record.errors[:updates] << "must be a hash" unless Hash === value
@@ -15,4 +16,18 @@ class Bulky::BulkUpdate < ActiveRecord::Base
   validates_each :ids do |record, attr, value|
     record.errors[:ids] << "must be an Array" unless Array === value
   end
+
+  scope :needs_notification, where(notified: false)
+
+  scope :belongs_to_user, lambda { |user_id| where("initiated_by_id = ? OR initiated_by_id IS ?", user_id, nil) }
+  
+  def mark_as_notified
+    self.notified = true
+    save!
+  end
+
+  # def self.retry(model, ids, updates, user_id)
+  #   Bulky.enqueue_update(model, ids, updates, user_id)
+  #   redirect_to bulky_edit_path(model), notice: I18n.t('flash.notice.enqueue_update')
+  # end
 end
