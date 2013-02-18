08fa098f71d27d47088cf4fbbeaad573a8581b1f
Zach Colon <zach.colon@tma1.com>
2013-02-18 16:46:47 -0500

    updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :bulky do
-    get ':model/edit', to: 'updates#edit',   as: 'edit'
-    put ':model',      to: 'updates#update', as: 'update'
+    get '/retry/:id',       to: 'admin/updates#retry',         as: 'retry'
+    get ':model/edit',      to: 'updates#edit',    as: 'edit'
+    put ':model',           to: 'updates#update',  as: 'update'
+    get ':id',              to: 'admin/updates#show',          as: 'show'
+    get '',                 to: 'admin/updates#index',         as: 'index'
   end
 end
