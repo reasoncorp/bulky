f203f640569a0433e9060fe9fb5eff8356f17ca0
Zach Colon <zach.colon@tma1.com>
2013-02-20 12:33:51 -0500

    Mark notified on retry

--- app/controllers/bulky/admin/updates_controller.rb
+++ app/controllers/bulky/admin/updates_controller.rb
@@ -16,6 +16,7 @@ class Bulky::Admin::UpdatesController < Bulky::ApplicationController
     updates = bulk_update.updates
     user_id = current_user.id
     Bulky.enqueue_update(model, ids, updates, current_user.id)
+    bulk_update.mark_as_notified
     redirect_to bulky_index_path(model: params[:model]), notice: I18n.t('flash.notice.enqueue_retry')
   end
 
