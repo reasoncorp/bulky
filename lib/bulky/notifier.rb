8d3627ed71aeaecae7003c4f06461c9a7ce7bb41
Zach Colon <zach.colon@tma1.com>
2013-02-20 12:36:47 -0500

    fixed flash notifications and removed unnecessary method

--- lib/bulky/notifier.rb
+++ lib/bulky/notifier.rb
@@ -1,18 +1,14 @@
 module Bulky
   module Notifier
-    
+   
     def self.included(klass)
       klass.before_filter :bulky_notifications
     end
     
     def bulky_notifications
       if Bulky::BulkUpdate.belongs_to_user(bulky_user_id).needs_notification.any?
-        flash.now[:notice] = %Q[<a href="/bulky/admin">Bulk Update Notifications</a>].html_safe
+        flash.now[:notice] = %Q[<a href="/bulky/admin">Bulk Update Notifications</a>].html_safe if flash[:notice].nil?
       end
-    end
-
-    def bulky_hide_notifications
-      @notifications = Bulky::BulkUpdate.needs_notification
     end
 
     def bulky_user_id
