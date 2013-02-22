5a21baed243cd6469fc29f04fbe3b7453b458fee
Zach Colon <zach.colon@tma1.com>
2013-02-22 13:30:15 -0500

    Revamp of retry action

--- app/controllers/bulky/admin/updates_controller.rb
+++ app/controllers/bulky/admin/updates_controller.rb
@@ -3,21 +3,18 @@ class Bulky::Admin::UpdatesController < Bulky::ApplicationController
   layout 'bulky'
 
   def index
-    bulk_updates
   end
 
   def show
-    bulk_update.mark_as_notified
+    bulk_update.mark_as_notified!
   end
 
   def retry
-    model = OpenStruct.new(name: bulk_update.updated_records.first.updatable_type)
-    ids = bulk_update.ids
-    updates = bulk_update.updates
-    user_id = current_user.id
-    Bulky.enqueue_update(model, ids, updates, current_user.id)
-    bulk_update.mark_as_notified
-    redirect_to bulky_index_path(model: params[:model]), notice: I18n.t('flash.notice.enqueue_retry')
+    begin
+      Bulky::Updater.perform(update_record.updatable_type, update_record.updatable_id, update_record.bulk_update_id)
+    rescue
+      redirect_to bulky_show_path(update_record.bulk_update_id), notice: I18n.t('flash.notice.enqueue_retry')
+    end
   end
 
   private
@@ -32,4 +29,7 @@ class Bulky::Admin::UpdatesController < Bulky::ApplicationController
   end
   helper_method :bulk_update
 
+  def update_record
+    @update_record ||= Bulky::UpdatedRecord.find(params[:id])
+  end
 end
