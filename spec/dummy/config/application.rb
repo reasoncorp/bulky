074eb883f41e46e1ef7318f174538731f7773580
adamhunter <adamhunter@me.com>
2015-08-17 14:54:11 -0400

    fixing specs wip

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,64 +1,31 @@
-require File.expand_path('../boot', __FILE__)
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../../../../Gemfile', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
+require 'rubygems'
+require 'bundler'
+
+Bundler.setup
+
+$:.unshift File.expand_path('../../../../lib', __FILE__)
+
+# for everything: require "rails/all"
 require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "active_record/railtie"
 
 Bundler.require
-require "bulky"
+
+ApplicationController = Class.new(ActionController::Base)
+
+require 'sqlite3'
+require 'bulky'
 
 module Dummy
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
-    # Enable escaping HTML in JSON.
-    config.active_support.escape_html_entities_in_json = true
-
-    # Use SQL instead of Active Record's schema dumper when creating the database.
-    # This is necessary if your schema can't be completely dumped by the schema dumper,
-    # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
-
-    # Enforce whitelist mode for mass assignment.
-    # This will create an empty whitelist of attributes available for mass-assignment for all models
-    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-    # parameters by using an attr_accessible or attr_protected declaration.
-    # config.active_record.whitelist_attributes = true
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+  class Application < ::Rails::Application
+    config.cache_classes = true
+    config.active_support.deprecation = :stderr
+    config.secret_token = 'http://s3-ec.buzzfed.com/static/enhanced/webdr03/2013/5/25/8/anigif_enhanced-buzz-11857-1369483324-0.gif'
+    config.eager_load = false
   end
 end
+
+Dummy::Application.initialize!
 
